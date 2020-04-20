c69cf9388e12818d676488d1434da720c87a6a5b
Liza George <liza@chiramattel.com>
2020-04-19 23:13:27 -0700

    Got a visible burger! Calling it a night for now tho.

--- react-complete-guide-burger-builder/react-complete-guide-burger-builder/src/containers/BugerBuilder/BurgerBuilder.js
+++ react-complete-guide-burger-builder/react-complete-guide-burger-builder/src/containers/BugerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import Burger from '../../components/Layout/Burger/Burger';
 
 class BurgerBuilder extends Component{
     render() {
         return(
             // <> is a shortcut for React.Fragment
             <>
-                <div>Burger</div>
+                <Burger/>
                 <div>Build Controls</div>
             </>
         );
